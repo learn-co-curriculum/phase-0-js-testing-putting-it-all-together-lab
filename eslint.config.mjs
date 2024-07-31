beb07dc5a892290c8a369a8ecd47aa1cab4b6326
Nick Christensen <nickachristensen@gmail.com>
2024-07-31 08:12:07 -0400

    Add eslint rule to ignore module.export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+const customGlobals = {
+  ...globals.browser,
+  module: 'readonly',
+};
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: customGlobals },
+  },
   pluginJs.configs.recommended,
 ];
